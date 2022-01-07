3a07ec975ba454c5d3957d89f0aba8ff26959945
silee706 <silee706@gmail.com>
2022-01-07 09:27:33 +0900

    message

---
+++ Catalogs/src/main/java/egovframework/msa/sample/serviceImpl/CustomerApiServiceImpl.java
@@ -0,0 +1,32 @@
+package egovframework.msa.sample.serviceImpl;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import egovframework.msa.sample.service.CustomerApiService;
+
+@Service
+public class CustomerApiServiceImpl implements CustomerApiService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Override
+    @HystrixCommand(fallbackMethod = "getCustomerDetailFallback")
+    public String getCustomerDetail(String customerId) {
+
+        // return restTemplate.getForObject("http://localhost:8082/customers/" +
+        // customerId, String.class);
+
+        return restTemplate.getForObject("http://customer/customers/" + customerId, String.class);
+        // throw new RuntimeException("I/O Exception");
+    }
+
+    public String getCustomerDetailFallback(String customerId, Throwable ex) {
+        System.out.println("Error:" + ex.getMessage());
+        return "고객정보 조회가 지연되고 있습니다.";
+    }
+}
